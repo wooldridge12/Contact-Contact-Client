b4e39414b1d09dcd95674d7d059169a7f3f06e24
wooldridge12 <wooldridgemikey@gmail.com>
2021-09-21 10:53:40 -0400

    Hover effects

--- src/components/aside/Aside.js
+++ src/components/aside/Aside.js
@@ -8,11 +8,11 @@ export const Aside = () => {
 
     return (
         <aside class="menu">
-  <p class="menu-label">
+  <p class="menu-label flexbox">
     Help Section
   </p>
   <ul class="menu-list">
-      <button onClick={() => {
+      <button class="hs-btn" onClick={() => {
         history.push({ pathname: "/help"})
       }}>Request Help</button>
       <div>Need help with anything? Dangerous thoughts? questions about 1st Civ Div? Or just someone to help you through? Click here. We all at some point need help figuring things out just because you hung up your uniform doesnt mean you left everything behind. We understand and have a battle buddy system to help you through this hard time you may be facing, Or to just answer questions abdout the work force, school, bills, living,... </div>
@@ -23,7 +23,7 @@ export const Aside = () => {
   <ul class="menu-list">
 
 
-  <button onClick={() => {
+  <button class="bb-btn" onClick={() => {
     return fetch("http://127.0.0.1:8000/battlebuddies", {
       method: "POST",
       headers: {
@@ -40,7 +40,7 @@ export const Aside = () => {
     })
   }
 } > Become A Battle Buddy</button>
-      <div> Help someone in need</div>
+      <div> Help someone in need, become a battle buddy. Do you have wisdom to pass to someone in need or feeling lost? perfect try to become a battle buddy. Suicide is often preventible. We survive combat zones with our battle buddies lets not stop now! We all struggle and survive to tell the tale. </div>
 </ul>
 </aside>
 )}
